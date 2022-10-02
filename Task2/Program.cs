9bfa6dbed7105009ea07ea22e9a94863de5236fe
Mariya <mariya.oboskakova@yandex.ru>
2022-10-02 15:55:39 +0700

    втарая задача

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine("Введите первое число");
+string firstNamber = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string secondNamber = Console.ReadLine();
+Console.WriteLine("Введите третье число");
+string thirdNamber = Console.ReadLine();
+
+int firstNum = int.Parse(firstNamber);
+int secondNum = int.Parse(secondNamber);
+int thirdNum = int.Parse(thirdNamber);
+int max = firstNum;
+
+
+if (firstNum > max ) max = firstNum;
+if (secondNum > max ) max = secondNum;
+if (thirdNum > max ) max = thirdNum;
+
+    Console.WriteLine(max);
