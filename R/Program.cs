f48cc8358f05e7286381ba48d00644ac86f0f649
Mariya <mariya.oboskakova@yandex.ru>
2022-10-02 15:54:40 +0700

    первая задача

--- R/Program.cs
+++ R/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.WriteLine("Введите первое число");
+string firstNamber = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string secondNamber = Console.ReadLine();
+
+int firstNum = int.Parse(firstNamber);
+int secondNum = int.Parse(secondNamber);
+
+if (firstNum > secondNum)
+
+    Console.WriteLine($"число {firstNum} является максимальным");
+
+else
+
+    Console.WriteLine($"число {secondNum} является максимальным");
