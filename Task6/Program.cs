d144ffa929c9c83f951114955a802b563520e4b3
Mariya <mariya.oboskakova@yandex.ru>
2022-10-07 14:50:17 +0700

    нахождение третьего числа

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+Console.WriteLine("введите число");
+int someNumber = int.Parse(Console.ReadLine());
+string anyNumberText = Convert.ToString(someNumber);
+
+if (someNumber > 3)
+{
+   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else
+{
+   Console.WriteLine($"нет третьего числа");
+}   
+
+    
