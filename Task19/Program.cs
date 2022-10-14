845b0a07bc552675086421eeb9ce3b3ff813e7a8
Mariya <mariya.oboskakova@yandex.ru>
2022-10-14 18:07:33 +0700

    задача 29

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число");
+int palidrom = int Parse(Console.ReadLine());
+int i = 0
+int palidromMax = 4;
+
+if (palidrom[i] == palidromMax; i++; palidromMax -1;)
+{
+     Console.WriteLine($" {i} ");
+} 
---
+++ Task29/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())*/
+
+Console.WriteLine("Введите элементы массива:");
+string[]arrayA = Console.ReadLine().Split(",");
+
+if (arrayA.Length != 8)
+{
+    int[]digitalArray = new int[arrayA.Length];
+    for (int i = 0; i < arrayA.Length; i++)
+    {
+        digitalArray[i] = Int32.Parse(arrayA[i]);
+    }
+}
+Console.WriteLine("[{0}]", string.Join(", ", arrayA));
