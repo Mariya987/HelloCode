491c5ba8a9a250fa9ff7209c0298eef1e2e94fff
Mariya <mariya.oboskakova@yandex.ru>
2022-10-25 19:52:35 +0700

    задача 38

---
+++ Task38/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76*/
+
+int[]RandomArray(int arraySize)
+{
+    int[] array = new int[arraySize];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arraySize ; i++)
+    {
+        array[i] = rnd.Next(10, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+(int, int) GetNegativeAndPositive(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+   return (max, min);
+}
+
+Console.WriteLine($"Введите размер массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+int[] arr = RandomArray(arraySize);
+PrintArray(arr);
+
+(int GetNumber, int GetNumberTwo) = GetNegativeAndPositive(arr);
+
+Console.WriteLine($"Максимальное значение массива: "+(GetNumber));
+Console.WriteLine($"Минимальное значение массива: "+(GetNumberTwo));
+Console.WriteLine("Разница максимального и минимального:  "+(GetNumber - GetNumberTwo));
