d9370f7611485e3d5aaa09f833d0b35941b8a612
Mariya <mariya.oboskakova@yandex.ru>
2022-11-14 12:31:21 +0700

    задача 60

---
+++ Task60/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+
+int[,,] matrix3D = new int[2, 2, 2];
+
+void PrintMatrix(int[,,] matrix3D)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"{matrix3D[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] matrix)
+{
+    int count = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[k, i, j] += count;
+                count += 1;
+            }
+        }
+    }
+}
+
+FillArray(matrix3D);
+PrintMatrix(matrix3D);
